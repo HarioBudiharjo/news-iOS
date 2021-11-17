f4d8b29890d7ea858b0863a49a6f3746c685c414
Hario Budiharjo <hariobudiharjo@gmail.com>
2021-11-18 01:57:45 +0700

    feat(news): Make UI news and category

--- News-iOS/Modules/ListNews/View/ListNewsCell.swift
+++ News-iOS/Modules/ListNews/View/ListNewsCell.swift
@@ -9,15 +9,9 @@ import UIKit
 
 class ListNewsCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    @IBOutlet weak var newsImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
     
 }
