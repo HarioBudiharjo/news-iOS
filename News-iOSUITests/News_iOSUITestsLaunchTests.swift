35f26c582802547803ad5f02b2561d0d6ef188d6
Hario Budiharjo <hario.budiharjo@amarbank.co.id>
2021-11-18 01:29:09 +0700

    Initial Commit

---
+++ News-iOSUITests/News_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  News_iOSUITestsLaunchTests.swift
+//  News-iOSUITests
+//
+//  Created by Hario Budiharjo on 18/11/21.
+//
+
+import XCTest
+
+class News_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
